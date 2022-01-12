68aed4c83cd342618839d7b068b0bfb03703ee50
Gustavo Souza <gustavohdesouza@hotmail.com>
2022-01-12 19:26:15 -0300

    (Funcional) MEsh basico, Spiffs

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,51 @@
-#include <Arduino.h>
-#include "SPIFFS.h"
+/*******************************************************************
+ * Projeto: POINTS
+ * Criador: Gustavo Souza (Berlandev)
+ * Data: 11/01/2022
+ * 
+ *******************************************************************/
+
+#include "painlessMesh.h"
+#include "mesh.cpp"
+
+#define   MESH_PREFIX     "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+
+
+
+
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+  String msg = "(B) Mensagem do B ";
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("(B) Origem: Ponto B (iD: %u) msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("-->(B) startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("(B) Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf(" (B) Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
 
 void setup() {
 /*******************************************************************
@@ -7,7 +53,8 @@ void setup() {
  *******************************************************************/
   Serial.begin(9600);
   Serial.println("-----------------------------------------------------------------------------------------------------------------");
-  Serial.println("Serial Iniciado");
+  Serial.println("Lista de Serviços Iniciados:");
+  Serial.println("Comunicação Serial");
 /*******************************************************************
  * Iniciando Spiffs (Envio de arquivo)
  *******************************************************************/
@@ -15,9 +62,38 @@ void setup() {
     Serial.println("Ocorreu um erro iniciando o SPIFFS");
     return;
   }
-  Serial.println("Spiffs Iniciado");
+  Serial.println("-----------------------------------------------------------------------------------------------------------------");
+  Serial.println("SPIFFS");
+  Serial.println("Arquivos:");
+ // Listar arquivos
+  File root = SPIFFS.open("/");
+   File file = root.openNextFile();
+ 
+  while(file){
+      Serial.print("--FILE: ");
+      Serial.println(file.name());
+ 
+      file = root.openNextFile();
+  }
+      Serial.println("Nenhum arquivo encontrado");
+  /*******************************************************************
+ * Iniciando Mesh
+ *******************************************************************/
+  Serial.println("-----------------------------------------------------------------------------------------------------------------");
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+  Serial.println("-----------------------------------------------------------------------------------------------------------------");
+  Serial.println("Mesh");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  mesh.update();
+  delay(1000);
 }
